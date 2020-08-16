dd640d34ac95d4e7e38b621d55e6b68095959393
Suraj <2015suraj.pawar@ves.ac.in>
2020-08-16 12:04:29 -0400

    run setup.py, update group name

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(
-        name='ncsu_se_group4',
+        name='Group12',
         version='2020.08.16',
         description='Future of SE',
         author='Refer contributors section of this repo',
@@ -9,6 +9,6 @@ setup(
         license='MIT',
         packages=['none'],
         install_requires=[
-            'python',
-        ],
+            'python3',
+        ]
     )
