feb93aac0fb171dd22eaa8949618ab3537b54749
Suraj <2015suraj.pawar@ves.ac.in>
2020-08-16 11:56:54 -0400

    update setup.py and code/__init__.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+        name='ncsu_se_group4',
+        version='2020.08.16',
+        description='Future of SE',
+        author='Refer contributors section of this repo',
+        author_email='atiwari4@ncsu.edu',
+        license='MIT',
+        packages=['none'],
+        install_requires=[
+            'python',
+        ],
+    )
